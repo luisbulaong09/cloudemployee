1c8b1458268c3457512d64d54f9400428b73193c
luisbulaong <luis.bulaong@eastvantage.com>
2016-10-20 21:10:11 +0800

    Initial commit

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,10 @@
+<header class="container-fluid">
+    <div id="header-content" class="row">
+        <div class="col-md-8" id="getontop">GETONTOP</div>
+        <div class="col-md-4" id="social-icon-container">
+            <i title="facebook" class="fa fa-facebook-f fa-lg" aria-hidden="true"></i>
+            <i title="twitter" class="fa fa-twitter fa-lg" aria-hidden="true"></i>
+            <i title="youtube" class="fa fa-youtube-play fa-lg" aria-hidden="true"></i>
+        </div>
+    </div>
+</header>
